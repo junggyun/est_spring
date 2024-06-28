3b60046f695663f903a6346f967ead8c4ff4ea3d
onlyplsson <onlyplsson@gmail.com>
2024-06-28 10:24:53 +0900

    :tada: first commit

---
+++ src/main/java/com/study/est_spring/EstSpringApplication.java
@@ -0,0 +1,13 @@
+package com.study.est_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EstSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EstSpringApplication.class, args);
+    }
+
+}
