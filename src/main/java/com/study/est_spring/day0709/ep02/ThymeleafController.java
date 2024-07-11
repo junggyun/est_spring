4ec840f033acec7df62eeed5c568bdf1fa2e9658
onlyplsson <onlyplsson@gmail.com>
2024-07-11 15:22:37 +0900

    :sparkles: /index api 수정

--- src/main/java/com/study/est_spring/day0709/ep02/ThymeleafController.java
+++ src/main/java/com/study/est_spring/day0709/ep02/ThymeleafController.java
@@ -3,13 +3,14 @@ package com.study.est_spring.day0709.ep02;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class ThymeleafController {
 
     @GetMapping("/index")
-    public String index(Model model) {
-        model.addAttribute("name", "박사장");
+    public String index(@RequestParam("name") String name, Model model) {
+        model.addAttribute("name", name);
 
         return "index";
     }
